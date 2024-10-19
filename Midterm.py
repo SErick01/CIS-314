91f82652342c53d24f583931d2a0540c50e533e5
Samantha Erickson <serick01@rams.shepherd.edu>
2024-10-18 21:52:18 -0400

    Modified and Streamlined

--- Midterm.py
+++ Midterm.py
@@ -7,50 +7,59 @@ Description:
 """
 import os, shutil, timeit, os, glob
 
-def sortFiles():
-    count = 0
-    dir = "C:/Users/samma/OneDrive/Pictures/Camera Roll/2024"
-    extensions = {".heic":"HEIC_Photos",
-                ".jpg":"JPG_Photes",
-                ".mov":"MOV_Videos" }
+tally = 0
+directory = "C:/Users/samma/OneDrive/Pictures"
+extensions = {".gif":"GIF_Photos",   ".heic":"HEIC_Photos",
+                  ".jpeg":"JPEG_Photos", ".jpg":"JPG_Photos",
+                  ".mov":"MOV_Videos",   "png":"PNG_Photos",
+                  "webp":"WEBP_Photos" }
 
+def renameFiles(count, dir, ext):
+    myPics.sort(key = os.path.getctime)
     start = timeit.default_timer()
+    
+    for type, name in ext.items():
+        myPics = glob.glob(os.path.join(dir, type))
+        for index, path in enumerate(myPics):
+            newName = name + f"{str(index + 1).zfill(2)}" + type
+            newPath = os.path.join(dir, newName)
+            os.rename(path, newPath)
+            count += 1
+    
+    elapsed = timeit.default_timer() - start
+    printResults("renameFiles", count, elapsed)
+
+def sortFiles(count, dir, ext):
+    start = timeit.default_timer()
+
     for file in os.listdir(dir):
         count += 1
-        filePath = os.path.join(dir,file)
+        filePath = os.path.join(dir, file)
         if os.path.isfile(filePath):
             ext = os.path.splitext(file)[1].lower()
             if ext in extensions:
                 folder = extensions[ext]
-                folderPath = os.path.join(dir,folder)
-                os.makedirs(folderPath,exist_ok=True)
-                newPath = os.path.join(folderPath,file)
+                folderPath = os.path.join(dir, folder)
+                os.makedirs(folderPath,exist_ok = True)
+                newPath = os.path.join(folderPath, file)
                 shutil.move(filePath, newPath)
             else:
                 count -= 1
                 print(f"Skipped file:{file} because extension not listed.")
+    
     elapsed = timeit.default_timer() - start
+    printResults("sortFiles", count, elapsed)
 
-    print("\nAll related files have been organized.")
-    print("The total number of files moved is:",count)
-    print("The total time it took to organize is: %.9f seconds\n" % elapsed)
+def printResults(caller, sum, time):
+    if caller == "printResults":
+        print("Files renamed Successfully.")
+        print("The total number of files renamed is:", sum)
+        print("The total time it took to search is: %.9f seconds\n" % time)
+    elif caller == "sortFiles":
+        print("\nAll related files have been organized.")
+        print("The total number of files moved is:", sum)
+        print("The total time it took to organize is: %.9f seconds\n" % time)
 
-def renameFiles():
-    total = 0
-
-    dir = "C:/Users/samma/OneDrive/Pictures/iCloud Photos/Downloads/Sisters"
-    myPics = glob.glob(os.path.join(dir, "*.jpg"))
-    myPics.sort(key=os.path.getctime)
-
-    start = timeit.default_timer()
-    for index, path in enumerate(myPics):
-        newName = f"Sisters_{str(index + 1).zfill(2)}.jpg"
-        newPath = os.path.join(dir,newName)
-        os.rename(path,newPath)
-        total += 1
-    elapsed = timeit.default_timer() - start
-
-    print("Files renamed Successfully.")
-    print("The total number of files renamed is:",total)
-    print("The total time it took to search is: %.9f seconds\n" % elapsed)
-
+renameFiles(tally, directory, extensions)
+tally = 0
+sortFiles(tally, directory, extensions)
