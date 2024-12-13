b869eae4516c361e712396e6e0365354316b38b9
Samantha Erickson <serick01@rams.shepherd.edu>
2024-12-12 21:46:53 -0500

    update

--- FinalProject/finalproject/main.py
+++ FinalProject/finalproject/main.py
@@ -1,10 +1,26 @@
+# FinalProject - A file management application
+# Copyright (C) 2024 Samantha Ann Erickson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import sys
 from PyQt6.QtCore import Qt
-from actionControllerClass import ActionController
+from finalproject.actionControllerClass import ActionController
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit,
     QPushButton, QFileDialog, QCheckBox, QComboBox, QMessageBox, QScrollArea, QToolButton)
-from fileManagerClass import extensions, categories
+from finalproject.fileManagerClass import extensions, categories
 
 class MainWindow(QWidget):
 
@@ -135,8 +151,11 @@ class MainWindow(QWidget):
         self.controller.execute_action(action, source, destination, selectedExtensions, filePrefix, newFolderName)
         QMessageBox.information(self, "Success", f"Action '{action}' completed successfully.")
 
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
